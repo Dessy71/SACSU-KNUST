ef92aa42bbdfeb89c342bb877766891f2f3401ff
desmond kofi antwi <dkay71.71@gmail.com>
2025-01-20 13:49:52 +0000

    Update ds.js

--- ds.js
+++ ds.js
@@ -86,7 +86,7 @@ function handleSubmit(event) {
             // Hide the success message after 5 seconds
             setTimeout(() => {
                 successMessage.style.display = "none";
-            }, 5000); // Hide after 5 seconds
+            }, 1500); // Hide after 5 seconds
 
             // Clear the form fields
             event.target.reset();
